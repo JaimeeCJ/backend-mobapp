f2efb51995a92245d575ed591230a32fd224d18d
Alfredo Pina <83417934+Alfr3d022@users.noreply.github.com>
2024-11-02 21:28:51 -0300

    Teste

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     public function index(){
         //$users = User::all();
         //return UserResource::collection($users) 
-        return "abroba";
+        return "abrobaaa";
     }
 }
 
