bc5535d7cdf9e586d098c3eee29b5cb6ff246175
RichardNaoki <richardnaoki@hotmail.com>
2024-11-27 14:53:01 -0300

    Adicionado Pesquisa por Nome

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,13 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\PromotionController;
 use App\Http\Controllers\SearchController;
-use App\Http\Controllers\SearchProductController;
 
 Route::get('/users', [UserController::class, 'index']); 
 Route::get('/users/{id}', [UserController::class, 'show']); 
 Route::get('/products/{qty}', [ProductController::class, 'index']);
 Route::get('/promotion/{qty}', [PromotionController::class, 'index']); 
 Route::get('/products/search/{id}', [SearchController::class, 'search']);
-Route::get('/products/search/{query}', [SearchProductController::class, 'search']);
+Route::get('/products/searchText/{p_text}', [SearchController::class, 'searchText']);
 
 
 Route::get('/', function (){
